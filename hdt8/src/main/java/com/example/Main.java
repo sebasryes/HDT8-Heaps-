1d7552ddd1a0cd077e938bee00034a317dea9499
sebasryes <83566772+sebasryes@users.noreply.github.com>
2022-04-09 10:57:57 -0600

    Update Main.java

--- hdt8/src/main/java/com/example/Main.java
+++ hdt8/src/main/java/com/example/Main.java
@@ -1,17 +1,53 @@
-package com.example;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Vector;
+import java.util.stream.Stream;
 
-/**
- * Hello world!
- */
-public final class App {
-    private App() {
-    }
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Vector listaPacientes = new Vector<Paciente>();
+
+        //Para leer el archivo
+        Scanner read = new Scanner(System.in);
+        String archivoNombre;
+        System.out.println("Ingrese el nombre del archivo");
+        archivoNombre = read.next();
+        ArrayList<String> archivo = new ArrayList<>();
+        try {
+            Stream<String> lines = Files.lines(
+                    Paths.get(archivoNombre + ".txt"),
+                    StandardCharsets.UTF_8
+            );
+            lines.forEach(archivo::add);
+        } catch (IOException e) {
+            System.out.println("Ha ocurrido un error");
+        }
+
+        for (String linea : archivo) {
+            ArrayList<String> pacienteArray = new ArrayList();
+            for (String info : linea.trim().split(",")) {
+                pacienteArray.add(info);
+            }
+
+            listaPacientes.add(new Paciente(pacienteArray.get(0), pacienteArray.get(1), pacienteArray.get(2)));
+        }
+
+        VectorHeap colaPacientes = new VectorHeap(listaPacientes);
+        //NO SE CUAL DE LAS DOS USAR
+        //VectorHeapC colaPacientes = new VectorHeapC(listaPacientes);
+        //PriorityQueue colaPacientes = new PriorityQueue(listaPacientes);
+
+        Boolean continuar = true;
+
+        while (continuar) {
+            System.out.println(colaPacientes.remove());
+            System.out.println("Mostrar siguiente paciente? s/n");
+            String continuarText = read.next();
+            continuar = continuarText.equals("s") && !colaPacientes.isEmpty();
+        }
     }
 }
